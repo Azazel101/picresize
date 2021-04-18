35284593a7b065f4a2f0fc0c0bf0de36222acadb
roman <roman.slovak@gmail.com>
2021-04-18 14:02:07 +0200

    add resolution edit

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_icon = '😇'
 )
 
-def get_image_download_link(img, quality=50):
+def get_image_download_link(img, quality=50, resolution=0.5, filename=None):
 
     button_uuid = str(uuid.uuid4()).replace('-', '')
     button_id = re.sub('\d+', '', button_uuid)
@@ -45,23 +45,36 @@ def get_image_download_link(img, quality=50):
     buffered = io.BytesIO()
     if not img.mode == 'RGB':
         img = img.convert('RGB')
+    size = [int(resolution * s) for s in img.size]
+    img = img.resize(size)
     img.save(buffered, format="JPEG", quality=quality)
-    st.write(str(round(buffered.tell() / 1000,2)) + 'Kb')
 
+    name = 'comp_' + str(filename)
+
+    st.write('Compressed file : ')
+    st.write({"FileName" : name,
+            "Resolution": size,
+            "FileSize": str(round(buffered.tell() / 1000,2)) + 'Kb'
+            })
     img_str = base64.b64encode(buffered.getvalue()).decode()
-    href = custom_css + f'<a download="compressed.jpg" id="{button_id}" href="data:file/jpg;base64,{img_str}">Download</a>'
+
+    href = custom_css + f'<a download="{name}" id="{button_id}" href="data:file/jpg;base64,{img_str}"> Download </a>'
     return href
 
 st.header("Image Resize")
 fileUpload = st.file_uploader("Choose a file", type = ['jpg','jpeg', 'png'])
-quality = st.slider('Select image quality', 0, 100, (10))
-
 
 if fileUpload is not None:
     file = fileUpload.read()
     result = Image.open(fileUpload)
-    file_details = {"FileName" : fileUpload.name, "FileType": fileUpload.type, "Resolution": result.size}
-    st.write(file_details)
-    st.image(result, width = 200)
+    file_details = {"FileName" : fileUpload.name, "FileType": fileUpload.type,"FileSize": str(round(fileUpload.size / 1000,2)) + 'Kb' , "Resolution": result.size}
 
-    st.markdown(get_image_download_link(result,quality), unsafe_allow_html=True)
+    quality = st.slider('Select image quality (%)', 5, 100, (50))
+#    resolution = st.slider('Select resolution', round(result.width * 0.1), result.width,(round(result.width / 2)))
+    resolution = st.slider('Select resolution (%)', 10, 100,(50))
+    resolution = resolution / 100
+    st.write('Original file : ')
+    st.write(file_details)
+#    st.image(result, width = 200)
+
+    st.markdown(get_image_download_link(result,quality, resolution, fileUpload.name), unsafe_allow_html=True)
